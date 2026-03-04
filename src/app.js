f8fe513394be1f207152405faeff1843f70d4bac
Nick Moura <120116187+nickmoura@users.noreply.github.com>
2026-03-04 19:49:48 -0300

    fix:url-cors-corrigida
    Corrigi o CORS para aceitar 5173 e 5174 em localhost.

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: true,
+  origin: [`http://localhost:5173`,`http://localhost:5174`],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
