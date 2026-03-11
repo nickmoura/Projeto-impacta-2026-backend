19122e383f154c9e758c216e9946e427802869c8
Nick Moura <120116187+nickmoura@users.noreply.github.com>
2026-03-11 16:34:54 -0300

    correcao-CORS

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: [`http://localhost:5173`,`http://localhost:5174`, `https://cliniflow-dev.vercel.app/`],
+  origin: [`http://localhost:5173`,`http://localhost:5174`, `https://cliniflow-dev.vercel.app`],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
