68bd76bd2286141f7725ca052b6a4709b5335605
ingrid inucencio da silva <ingrid.painss@gmail.com>
2026-03-23 22:29:27 -0300

    feat: endpoint editar consulta
    :wq!#

--- src/routes/private.js
+++ src/routes/private.js
@@ -1,11 +1,21 @@
 import express from 'express';
+<<<<<<< HEAD
 import  dashboradController  from '../controllers/dashboardController.js';
 import  authMiddleware  from '../middleware/authMiddleware.js';
 import appointmentController from '../controllers/appointmentController.js';
+=======
+import dashboradController from '../controllers/dashboardController.js';
+import authMiddleware from '../middleware/authMiddleware.js';
+import editarConsulta from '../controllers/consultaController.js';
+>>>>>>> af41138 (feat: endpoint editar consulta)
 
 const router = express.Router();
 
 router.get('/dashboard', authMiddleware, dashboradController);
+<<<<<<< HEAD
 router.post('/appointments', authMiddleware, appointmentController.createAppointment);
+=======
+router.put('/consultas/:id', editarConsulta);
+>>>>>>> af41138 (feat: endpoint editar consulta)
 
 export default router;
