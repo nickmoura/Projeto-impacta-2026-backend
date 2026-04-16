f78e7f5c64241ebf8a4270c2758c1cae1fc76ecd
Nick Moura <120116187+nickmoura@users.noreply.github.com>
2026-04-16 15:13:14 -0300

    fix:TCC-126-dominio-no-cors
    Inseri os nossos domínios próprios no CORS.

--- src/app.js
+++ src/app.js
@@ -10,8 +10,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: [`http://localhost:5173`,`http://localhost:5174`, `https://cliniflow-dev.vercel.app`, `https://cliniflow-tcc.vercel.app
-`],
+  origin: [`http://localhost:5173`,`http://localhost:5174`, `https://cliniflow-dev.vercel.app`, `https://cliniflow-tcc.vercel.app`, `https://www.cliniflow.cloud`, `https://cliniflow.cloud`],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
