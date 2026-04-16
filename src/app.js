2a0ad6d0196301b081741f65ddff4df60d7464db
Nick Moura <120116187+nickmoura@users.noreply.github.com>
2026-04-16 15:48:31 -0300

    fix:TCC-126-ambiente-dev-add-cors

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: [`http://localhost:5173`,`http://localhost:5174`, `https://cliniflow-dev.vercel.app`, `https://cliniflow-tcc.vercel.app`, `https://www.cliniflow.cloud`, `https://cliniflow.cloud`],
+  origin: [`http://localhost:5173`,`http://localhost:5174`, `https://cliniflow-dev.vercel.app`, `https://cliniflow-tcc.vercel.app`, `https://www.cliniflow.cloud`, `https://cliniflow.cloud`, `https://dev.cliniflow.cloud`],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
